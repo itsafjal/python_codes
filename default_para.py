36af99db195479da03681dc891cbd884aafb1872
Mohammad Afjal <afjal321@gmail.com>
2016-06-29 23:33:48 +0530

    Add files via upload

---
+++ default_para.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+class start():
+	def __init__(self, x=0, y=0):
+		self.x=x
+		self.y=y
+
+
+p1=start(4,5)
+p=start()
+print p1.x, p1.y
+print p.x, p.y
---
+++ sys.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+import sys
+import os
+os.system('echo \'hello world\'')
+
+print sys.platform
